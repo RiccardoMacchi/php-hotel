32025bc15b52b94a1c19ce4a1c17a90eaff0c817
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-24 15:21:37 +0200

    add files stampa in pagina dati hotels

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <table>
+            <tr>
+                <th>Nome Hotel</th>
+                <th>Descrizione Hotel</th>
+                <th>Disponibile Parcheggio</th>
+                <th>Voto</th>
+                <th>Distanza dal centro in km</th>
+            </tr>
+            <?php foreach($hotels as $hotel): ?>
+            <tr>
+                <td><?php echo $hotel['name'] ?></td>
+                <td><?php echo $hotel['description'] ?></td>
+                <td><?php echo ($hotel['parking'] ? 'Si' : 'No') ?></td>
+                <td><?php echo $hotel['vote']?></td>
+                <td><?php echo $hotel['distance_to_center'] ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+</body>
+</html>
